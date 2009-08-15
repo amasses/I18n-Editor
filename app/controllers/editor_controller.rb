9d7a43b212c7dbaa02435889c8191be0f79aba7f
Matthew Savage <Broc@Broc.local>
2009-08-16 02:48:50 +1000

    Added css, view for renaming translation etc Added backups for selected files Updated to use AR session store because lots of selected files overflowed the cookie store - should consider just adding the file list to a static file in /tmp and reading that when needed - or implement a yaml session store

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,3 +1,5 @@
+require 'ftools'
+
 class EditorController < ApplicationController
   def choose_directory
   end
@@ -19,6 +21,14 @@ class EditorController < ApplicationController
     end
 
     session[:files] = params[:selected_files]
+    # Backup files!
+    params[:selected_files].each do |file|
+      File.copy(file, "#{file}.backup")
+    end
+
+    directory = File.dirname(params[:selected_files].first)
+    system("tar", "zcvf", "#{directory}/backup_#{Time.now.to_formatted_s(:short).gsub(/[^a-z0-9]/i, "_")}.tgz", "#{directory}/*.backup")
+
     redirect_to "/editor/show_translations"
   end
 
@@ -41,6 +51,31 @@ class EditorController < ApplicationController
   end
 
   def rename_translation
+    @translation = params[:key]
+  end
+
+  def save_rename_translation
+    if params[:new_translation_name].nil? or params[:new_translation_name].empty?
+      flash[:notice] = "Sorry, you need to provide a new translation name."
+      @translation = params[:original_key]
+      render :rename_translation and return
+    end
+
+    key = params[:original_key]
+    new_key = params[:new_translation_name]
+    
+    files = session[:files]
+    files.each do |file|
+      yaml = YAML.load_file file
+      yaml.each_pair do |lang, translations|
+
+          orig = translations.delete(key)
+          translations[key] = orig
+      end
+
+      
+    end
+    
   end
 
   def save_translation
